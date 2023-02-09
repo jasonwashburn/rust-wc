0bb905062f35f3b4ab9d544faabdf274d5067beb
Jason Washburn <jason.washburn@gmail.com>
2023-02-08 20:47:20 -0600

    adds argument parsing function

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,56 @@
 use std::io::{Read, Result};
 use std::{env, io};
 
-fn main() -> Result<()> {
-    let mut count_bytes = false;
-    let mut count_lines = false;
-    let args: Vec<String> = env::args().collect();
-    if args.len() > 1_usize {
-        for arg in args.iter().skip(1) {
-            match arg.as_str() {
-                "-c" => count_bytes = true,
-                "-l" => count_lines = true,
-                _ => (),
-            }
+struct FlagOptions {
+    count_lines: bool,
+    count_bytes: bool,
+}
+
+impl FlagOptions {
+    fn new() -> Self {
+        FlagOptions {
+            count_lines: false,
+            count_bytes: false,
         }
     }
+}
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let flag_options = match parse_args(args) {
+        Ok(x) => x,
+        Err(e) => {
+            println!("{e}");
+            std::process::exit(1);
+        }
+    };
     let mut buffer = String::new();
     let byte_count = io::stdin().read_to_string(&mut buffer)?;
-    if count_bytes {
+    if flag_options.count_bytes {
         print!("\t{byte_count}");
     }
     let line_count = buffer.lines().count();
-    if count_lines {
-        println!("\t{line_count}");
+    if flag_options.count_lines {
+        print!("\t{line_count}");
     }
+    println!();
     Ok(())
+}
+
+fn parse_args(args: Vec<String>) -> Result<FlagOptions> {
+    let mut flag_options = FlagOptions::new();
+
+    for arg in args.iter().skip(1) {
+        match arg.as_str() {
+            "-c" => flag_options.count_bytes = true,
+            "-l" => flag_options.count_lines = true,
+            _ => {
+                return Err(std::io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    format!("Unrecognized Argument: {arg}"),
+                ))
+            }
+        }
+    }
+    Ok(flag_options)
 }
